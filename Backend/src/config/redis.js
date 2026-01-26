53b6980bd04f131aa6353f88b761351eb85fabb2
Asif Ansari <asifstudy123@gmail.com>
2026-01-26 21:18:23 +0530

    fixed Redis DB

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -1,14 +1,17 @@
 import { createClient } from "redis";
 import "dotenv/config";
 
-const redisClient = createClient({
-  username: "default",
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+const redisClient = createClient(
+  {
+    username: "default",
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    },
   },
-});
+  console.log("Redis Connected"),
+);
 
 redisClient.on("error", (err) => {
   console.error("Redis Client Error:", err);
